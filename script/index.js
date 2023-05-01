e90936bfb75dc2283279fe7853211f7d33a73ff4
Avbarkova <avbarkova@gmail.com>
2023-05-01 19:14:14 +0300

    feat: implemented keyboard layout switching

--- script/index.js
+++ script/index.js
@@ -1,4 +1,88 @@
 import onloadKeybord from '../script/onloadKeybord.js';
-
+import keyboardElementEn from '../script/keyboardElementEn.js';
+import keyboardElementRu from '../script/keyboardElementRu.js';
+let extra = ['Backspace', 'Tab', 'Delete', 'CapsLock', 'Enter', 'ShiftLeft', 'ShiftRight', 'ControlLeft', 'ControlRight', 'MetaLeft', 'AltLeft', 'AltRight', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+// загрузка DOM-элементов
+let language;
+if (localStorage.getItem('language')) {
+  language = localStorage.getItem('language');
+} else {
+  language ='en';
+  localStorage.setItem('language', language);
+}
+main();
+function main() {
 onloadKeybord();
-
+let textarea = document.querySelector('textarea');
+let buttons = document.querySelectorAll('.key_btn');
+// клик по виртуальной клавиатуре выделяет кнопку
+for (let button of buttons) {
+  button.addEventListener('mouseup', (e) => {
+    if (language === 'en') {
+      for (let elem of keyboardElementEn) { //английский символ отражается в поле ввода при клике мыши
+        if (e.target.id === elem.code) {
+          if (!extra.includes(e.target.id)) {
+            textarea.value += elem.symbol;
+          }
+        }
+      }
+    } else {
+      for (let elem of keyboardElementRu) { //русский символ отражается в поле ввода при клике мыши
+        if (e.target.id === elem.code) {
+          if (!extra.includes(e.target.id)) {
+            textarea.value += elem.symbol;
+          }
+        }
+      }
+    }
+  });
+}
+// нажатие на кнопку физической клавиатуры выделяет кнопку на виртуальной
+document.addEventListener('keydown', (e) => {
+  e.preventDefault();// нажатие на кнопку физической клавиатуры прерывает обработчик событий при активном
+  for (let button of buttons) {
+    if (button.id === e.code) {
+      button.classList.add('active');
+    }
+  }
+  // переключение языка
+  if (e.code === 'ShiftLeft') {
+    document.addEventListener('keyup', (e) => {
+      if (e.code === 'ControlLeft') {
+        if (language === 'en') {
+          language = 'ru';
+        } else {
+          language = 'en'
+        }
+        localStorage.setItem('language', language);
+        document.body.innerHTML = '<script type="module" src="./script/index.js" defer></script>';
+        main();
+      }
+    });
+  }
+});
+document.addEventListener('keyup', (e) => {
+  for (let button of buttons) { // стирает выделение клавиши
+    if (e.code === button.id) {
+      button.classList.remove('active');
+    }
+  }
+  if (language === 'en') {
+    for (let elem of keyboardElementEn) { //английский символ отражается в поле ввода при клике мыши
+      if (e.code === elem.code) {
+        if (!extra.includes(e.code)) {
+          textarea.value += elem.symbol;
+        }
+      }
+    }
+  } else {
+    for (let elem of keyboardElementRu) { //русский символ отражается в поле ввода при клике мыши
+      if (e.code === elem.code) {
+        if (!extra.includes(e.code)) {
+          textarea.value += elem.symbol;
+        }
+      }
+    }
+  }
+});
+};
--- script/onloadKeybord.js
+++ script/onloadKeybord.js
@@ -1,21 +1,34 @@
 import keyboardElementEn from './keyboardElementEn.js';
 import keyboardElementRu from './keyboardElementRu.js';
 import elementSequence from './elementSequence.js';
-// create buttons of keyboard for wrapper
+// создание кнопок клавиатуры для wrapper
 let onloadKeybord = function onload(){
   function fillKeyboardWrapper() {
     let content = '';
     for (let i=0; i<elementSequence.length; i++) {
+      if (localStorage.getItem('language') === 'en') {
         for (let element of keyboardElementEn) {
-            if (element.code === elementSequence[i]) {
-              // check if the element contains a class
-              if (element.class) {
-                content += `<div class="key_btn ${element.class}">`+element.symbol+'</div>'
-              } else {
-                content += '<div class="key_btn">'+element.symbol+'</div>'
-              }
+          if (element.code === elementSequence[i]) {
+            // проверем, есть ли у кнопки дополнительный класс для добавления его
+            if (element.class) {
+              content += `<div class="key_btn ${element.class}" id="${element.code}">`+element.symbol+'</div>'
+            } else {
+              content += `<div class="key_btn" id="${element.code}">`+element.symbol+'</div>'
             }
-        }   
+          }
+        } 
+      } else {
+        for (let element of keyboardElementRu) {
+          if (element.code === elementSequence[i]) {
+            // проверем, есть ли у кнопки дополнительный класс для добавления его
+            if (element.class) {
+              content += `<div class="key_btn ${element.class}" id="${element.code}">`+element.symbol+'</div>'
+            } else {
+              content += `<div class="key_btn" id="${element.code}">`+element.symbol+'</div>'
+            }
+          }
+        } 
+      }
     }
     return content;
   }
@@ -25,7 +38,7 @@ let onloadKeybord = function onload(){
   header.className = 'header';
   header.innerHTML='<div class="container"><h1>Microsoft Windows virtual keyboard</h1></div>';
   const main = document.createElement('main');
-  main.innerHTML = `<div class="container"><textarea class="textarea"></textarea><p class="switch-language">Press Shift-left + Ctrl-left to change language</p><div class="keyboard__wrapper">${content}</div></div>`;
+  main.innerHTML = `<div class="container"><textarea class="textarea"></textarea><p class="switch-language">Press Shift-left + Alt-left to change language</p><div class="keyboard__wrapper">${content}</div></div>`;
   document.body.append(header);
   document.body.append(main);
 };
